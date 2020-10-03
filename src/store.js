29d4611ae64944b5e6c95eab079f79349b99b00c
Salman <salmanhammed77@gmail.com>
2020-10-03 22:02:35 +0530

    refactor

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 
 import { writable, derived } from 'svelte/store';
+import { formatTimeStr } from "./utils.js";
 
 export const alertObj = writable({});
 
-export const message = writable("I am writing code in Wix.");
+export const message = writable("");
 export const createdAt = writable(new Date());
 export const nextAlert = writable(new Date());
 export const currentTime = writable(new Date());
@@ -12,23 +13,3 @@ export const alertBox = writable(false);
 export const timeString = derived([nextAlert, currentTime], formatTimeStr);
 export const interval = writable(undefined);
 
-
-function formatTimeStr([$nextAlert, $currentTime]) {
-
-  let string = "";
-
-  const total = Math.max( ( Date.parse($nextAlert) - Date.parse(new Date()) ), 0 );
-  const seconds = Math.floor( (total/1000) % 60 );
-  const minutes = Math.floor( (total/1000/60) % 60 );
-  const hours = Math.floor( (total/(1000*60*60)) % 24 );
-  const days = Math.floor( total/(1000*60*60*24) );
-
-  const isMorethanHour = (hours) >= 1;
-  const isMorethanMin = (minutes) >= 1;
-
-  if( isMorethanHour ) string = `${hours}h`;
-  if( isMorethanMin ) string += ` ${minutes}m`;
-  
-  string += ` ${ seconds }s`;
-  return { string, total };
-}
--- src/utils.js
+++ src/utils.js
@@ -11,3 +11,23 @@ export function playAlert() {
     setTimeout(()=>{audio.play();   res() }, 1200 );
   });
 }
+
+export function formatTimeStr([$nextAlert, $currentTime]) {
+
+  let string = "";
+
+  const total = Math.max( ( Date.parse($nextAlert) - Date.parse(new Date()) ), 0 );
+  const seconds = Math.floor( (total/1000) % 60 );
+  const minutes = Math.floor( (total/1000/60) % 60 );
+  const hours = Math.floor( (total/(1000*60*60)) % 24 );
+  const days = Math.floor( total/(1000*60*60*24) );
+
+  const isMorethanHour = (hours) >= 1;
+  const isMorethanMin = (minutes) >= 1;
+
+  if( isMorethanHour ) string = `${hours}h`;
+  if( isMorethanMin ) string += ` ${minutes}m`;
+  
+  string += ` ${ seconds }s`;
+  return { string, total };
+}
